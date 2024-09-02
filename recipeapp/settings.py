a8c758a1c4e29d3a6bb3b1f3b2471ef2503e04fb
KatieB5 <katie.bickford2@gmail.com>
2024-09-02 15:48:32 +0100

    added formatting for signup form

--- recipeapp/settings.py
+++ recipeapp/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     #local apps
     'recipe_app',
     'users',
+
+    #3rd party
+    'crispy_forms',
+    'crispy_bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
