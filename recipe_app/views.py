18d3a1cf0b19d916adacea807e0e1cc1e0a2da8a
KatieB5 <katie.bickford2@gmail.com>
2024-09-02 11:11:12 +0100

    set up initial home and about views

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Welcome to the recipes app!")
+
+def about(request):
+    return HttpResponse("This is a recipes app to keep track of your recipes.")
