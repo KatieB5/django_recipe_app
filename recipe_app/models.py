1eb2abade74499a71c079339431bc780aa5b97f6
KatieB5 <katie.bickford2@gmail.com>
2024-09-02 14:58:45 +0100

    updates to pull data from database

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -10,4 +10,5 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
-    
+    def __str__(self):
+        return self.title
--- recipe_app/views.py
+++ recipe_app/views.py
@@ -1,29 +1,9 @@
 from django.shortcuts import render, HttpResponse
-
-#Dummy data
-recipes = [
-    {
-        'author': 'Adam',
-        'title': 'Baked Oat Bars',
-        'directions': 'Throw all ingredients into a bowl and stir to combine, then bake.',
-        'date_posted': 'May 19, 2024'
-    },
-    {
-        'author': 'Benji',
-        'title': 'Carbonara',
-        'directions': 'Throw all ingredients into a pan, hope for the best.',
-        'date_posted': 'June 19, 2024'
-    },
-    {
-        'author': 'Carla',
-        'title': 'Double chocolate fudge cake',
-        'directions': 'Mix ingredients, bake for 40 mins then eat the whole thing.',
-        'date_posted': 'July 19, 2024'
-    },
-]
+from . import models
 
 # Create your views here.
 def home(request):
+    recipes = models.Recipe.objects.all()
     context = {
         'recipes': recipes
     }
