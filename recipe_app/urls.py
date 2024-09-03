79356680b8f1fd85f8590695b9bfbafed39862f9
KatieB5 <katie.bickford2@gmail.com>
2024-09-03 09:42:03 +0100

    added update recipe functionality

--- recipe_app/urls.py
+++ recipe_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.RecipeListView.as_view(), name='home'),
     path('recipe/<int:pk>/', views.RecipeDetailView.as_view(), name='detail'),
     path('recipe/create/', views.RecipeCreateView.as_view(), name='create'),
+    path('recipe/<int:pk>/update/', views.RecipeUpdateView.as_view(), name='update'),
     path('about/', views.about, name='about'),
 ]
